0f881a73088eb829219d91f10edbd7e2e532357f
Evgeniy <evsinev@gmail.com>
2013-02-16 16:59:27 +0400

    added upload wiki files to redmine

---
+++ src/main/java/com/payneteasy/firewall/MainWikiDir.java
@@ -0,0 +1,45 @@
+package com.payneteasy.firewall;
+
+import com.google.common.base.Preconditions;
+import com.google.common.io.Files;
+import com.payneteasy.firewall.redmine.RedmineEasyClient;
+import com.payneteasy.firewall.service.IWikiService;
+import com.payneteasy.firewall.service.impl.WikiServiceImpl;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ */
+public class MainWikiDir {
+    public static void main(String[] args) throws KeyManagementException, NoSuchAlgorithmException, IOException {
+        if(args.length<3) {
+            System.out.println("usage: MainWikiDir redmine-url redmine-key wiki-dir");
+            System.exit(1);
+        }
+
+        String redmineUrl = args[0];
+        String redmineKey = args[1];
+        File wikiDir    = new File(args[2]);
+
+        Preconditions.checkArgument(wikiDir.exists(), "Dir %s is not exists", wikiDir.getAbsolutePath());
+
+        File[] files = wikiDir.listFiles();
+
+        RedmineEasyClient redmineClient = new RedmineEasyClient(redmineUrl, redmineKey);
+
+        for (File file : files) {
+            System.out.println(file+"...");
+            redmineClient.executeCreateOrUpdateWikiPage(
+                    file.getName() // page name
+                    , file.getName() // title
+                    , Files.toString(file, Charset.defaultCharset())
+                    , "update info from server"
+            );
+        }
+    }
+}
